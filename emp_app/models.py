bc7f1d0c1cdf47c63acf016179b9277d80e68277
GAURAVSOLANKI421 <gauravsolanki421@gmail.com>
2023-05-14 22:44:47 +0530

    View all employees

--- emp_app/models.py
+++ emp_app/models.py
@@ -5,8 +5,22 @@ class Department(models.Model):
     name = models.CharField(max_length=200,null=False)
     location= models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+    
+
+
+
+
 class Role(models.Model):
     name = models.CharField(max_length=200,null=False)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=200,null=False)
@@ -15,6 +29,11 @@ class Employee(models.Model):
     salary= models.IntegerField(default=0)
     bonus= models.IntegerField(default=0)
     role = models.ForeignKey(Role,on_delete=models.CASCADE)
+    #location= models.ForeignKey(Department,on_delete=models.CASCADE,related_name='location')
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
+
+
+    def __str__(self):
+        return "%s,%s,%s"%(self.first_name,self.last_name,self.phone)
 
--- emp_app/urls.py
+++ emp_app/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('all_emp',views.all_emp,name='all_emp'),
+    path('add_emp',views.add_emp,name='add_emp'),
+    path('remove_emp',views.remove_emp,name='remove_emp'),
+    path('filter_emp',views.filter_emp,name='filter_emp')
+    
     
 ]
