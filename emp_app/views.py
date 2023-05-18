7271e0a923e9e2400b84639df44f12f2905bd8d6
GAURAVSOLANKI421 <gauravsolanki421@gmail.com>
2023-05-18 21:39:53 +0530

    add table rows from form input

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render ,HttpResponse
 from .models import Employee,Department,Role
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -13,7 +14,26 @@ def all_emp(request):
 
 
 def add_emp(request):
-    return render(request,'add_emp.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept = request.POST['dept']
+        role = request.POST['role']
+
+        new_emp = Employee(first_name=first_name, last_name=last_name,salary=salary,bonus=bonus,phone=phone,dept_id=dept,role_id=role,hire_date=datetime.now())
+        new_emp.save()
+
+        return HttpResponse('Added successfully')
+
+    elif request.method== 'GET':
+        return render(request,'add_emp.html')
+    
+    else:
+        return HttpResponse('Something went wrong')
+
 
 
 def remove_emp(request):
