be754158897e3b63cc4e5f2ac599673535e0f071
GAURAVSOLANKI421 <gauravsolanki421@gmail.com>
2023-05-28 13:16:22 +0530

    Final commit , all 4 modules done

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render ,HttpResponse
 from .models import Employee,Department,Role
 from datetime import datetime
-
+from django.db.models import Q
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -23,6 +23,8 @@ def add_emp(request):
         dept = request.POST['dept']
         role = request.POST['role']
 
+        # since dept and role is foreign key , will use as dept_id and role_id
+
         new_emp = Employee(first_name=first_name, last_name=last_name,salary=salary,bonus=bonus,phone=phone,dept_id=dept,role_id=role,hire_date=datetime.now())
         new_emp.save()
 
@@ -36,8 +38,39 @@ def add_emp(request):
 
 
 
-def remove_emp(request):
-    return render(request,'remove_emp.html')
+def remove_emp(request , emp_id=0):
+    if emp_id:
+        try:
+            emp_to_delete = Employee.objects.get(id=emp_id)
+            emp_to_delete.delete()
+            return HttpResponse('Employee removed successfully')
+        except:
+            return HttpResponse('Please Enter a valid ID')
+    emps = Employee.objects.all()
+    context = {'emps':emps}
+
+
+    return render(request,'remove_emp.html',context)
+
 
 def filter_emp(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        dept = request.POST['dept']
+        role = request.POST['role']
+        emps = Employee.objects.all()
+        if name:
+            emps= emps.filter(Q(first_name__icontains =name)|Q(last_name__icontains =name))
+        if dept:
+            emps =emps.filter(dept__name=dept)
+        if role:
+            emps = emps.filter(role__name=role)
+        context ={'emps':emps}
+        return render(request,'all_emp.html',context)
+
+    elif request.method=='GET':
+        return render(request , 'filter_emp.html')
+    else:
+        return HttpResponse("No filter available")
+
     return render(request,'filter_emp.html')
